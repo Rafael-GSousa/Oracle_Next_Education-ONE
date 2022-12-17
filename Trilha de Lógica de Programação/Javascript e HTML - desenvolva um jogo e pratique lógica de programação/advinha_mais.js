fbe7fef44da677a65a0b028db9eef6ed97bd8b09
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-12-17 19:17:00 -0300

    update advinha_mais.js

--- Trilha de Lógica de Programação/Javascript e HTML - desenvolva um jogo e pratique lógica de programação/advinha_mais.js
+++ Trilha de Lógica de Programação/Javascript e HTML - desenvolva um jogo e pratique lógica de programação/advinha_mais.js
@@ -1,54 +1,54 @@
-    function sorteia(){
+function sorteia(){
 
-        return Math.round(Math.random() * 1000);
-    }
+    return Math.round(Math.random() * 1000);
+}
 
-    function sorteiaNumeros(quantidade){
-        var segredos = [];
-        var numero = 1;
+function sorteiaNumeros(quantidade){
+    var segredos = [];
+    var numero = 1;
 
-        while(numero <= quantidade){
-            var numeroAleatorio = sorteia();
+    while(numero <= quantidade){
+        var numeroAleatorio = sorteia();
 
-            if(numeroAleatorio != 0){
-                var achou = false;
+        if(numeroAleatorio != 0){
+            var achou = false;
 
-                for(var pos = 0; pos < segredos.length; pos++){
-                    if(segredos[pos] == numeroAleatorio){
-                        achou = true;
-                        break;
-                    }
-                }
-                if(!achou){
-                    segredos.push(numeroAleatorio);
-                    numero++;
+            for(var pos = 0; pos < segredos.length; pos++){
+                if(segredos[pos] == numeroAleatorio){
+                    achou = true;
+                    break;
                 }
             }
+            if(!achou){
+                segredos.push(numeroAleatorio);
+                numero++;
+            }
         }
-        return segredos;
     }
-    
-    var segredos = sorteiaNumeros(parseInt(prompt("Quantos valores no array:")));
-    console.log(segredos);
-  
-    var input = document.querySelector("input");
+    return segredos;
+}
+
+var segredos = sorteiaNumeros(parseInt(prompt("Quantos valores no array:")));
+console.log(segredos);
+
+var input = document.querySelector("input");
+input.focus();
+function verifica(){
+    var achou = false;
+    for(var pos = 0; pos < segredos.length; pos++){
+        if(input.value == segredos[pos]){
+            achou = true;
+            alert("Você acertou!");
+            break;
+        }
+    }
+
+    if(!achou){
+        alert("Você errou!")
+    }
+    input.value = "";       
     input.focus();
-    function verifica(){
-        var achou = false;
-        for(var pos = 0; pos < segredos.length; pos++){
-            if(input.value == segredos[pos]){
-                achou = true;
-                alert("Você acertou!");
-                break;
-            }
-        }
-
-        if(!achou){
-            alert("Você errou!")
-        }
-        input.value = "";       
-        input.focus();
-        
-    }
-    var button = document.querySelector("button");
-    button.onclick = verifica;
+    
+}
+var button = document.querySelector("button");
+button.onclick = verifica;
