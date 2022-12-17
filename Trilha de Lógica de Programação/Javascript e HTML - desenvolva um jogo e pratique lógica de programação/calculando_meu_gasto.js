24f89b844ace6b08f2cb4a1d93a834f4265c7de5
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-12-17 19:35:35 -0300

    calculando_meu_gasto.js

---
+++ Trilha de Lógica de Programação/Javascript e HTML - desenvolva um jogo e pratique lógica de programação/calculando_meu_gasto.js
@@ -0,0 +1,4 @@
+document.write("A média do valor gasto com as viagens é ") ;
+document.write( (200 + 100 + 300 + 400) / 4);
+document.write("<br>");
+document.write("A" + "B" + 20 + 10 + "C" + (5 + 10) + "D");
