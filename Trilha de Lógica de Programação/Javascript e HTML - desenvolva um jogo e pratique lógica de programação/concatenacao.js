8920331eedef86326ea234a5fb7c4cca96e0efc8
Rafael-GSousa <rafael.gsousa@outlook.com>
2022-12-17 19:38:18 -0300

    create concatenacao.js

---
+++ Trilha de Lógica de Programação/Javascript e HTML - desenvolva um jogo e pratique lógica de programação/concatenacao.js
@@ -0,0 +1,10 @@
+document.write("A idade do Flávio é ");
+document.write("18");
+document.write("<br>");
+document.write("A idade do Flávio é <br> 18");
+document.write("<br>");
+document.write("18" + "20");
+document.write("<br>"); 
+document.write("18" + 20);
+document.write("<br>");
+document.write(2016 - 39);
