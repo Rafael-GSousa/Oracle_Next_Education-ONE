d8925532ba561a93ce4a6179ebc230acade0b056
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-27 18:34:15 -0300

    Adicionando Tarefa

--- Trilha Front End/JavaScript na Web - manipule o DOM com JavaScript/listaDeTarefas.js
+++ Trilha Front End/JavaScript na Web - manipule o DOM com JavaScript/listaDeTarefas.js
@@ -1,9 +1,15 @@
 const criarTarefa = (evento) => {
     evento.preventDefault();
-    const input = document.querySelector('[data-form-input]');
-    const valor = input.value;
-    console.log(valor);
-    input.value = " ";
+    const input = document.querySelector('[data-form-input]'); //acessando o input
+    const valor = input.value; //conteúdo do input
+
+    const tarefa = document.querySelector('[data-task]'); //acessando o parágrafo do <li>
+
+    const conteudo = `<p class="content">${valor}</p>`; //criando um novo parágrafo com conteúdo
+
+    tarefa.innerHTML = conteudo; //adicionando o novo parágrafo criado no <li>
+
+    input.value = " "; //limpa o input
 }
 
 const novaTarefa = document.querySelector('[data-form-button]');
