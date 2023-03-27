06b3587ef879b08e395c68f3ccad2440f958b2c4
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-27 19:00:27 -0300

    Criando elemento

--- Trilha Front End/JavaScript na Web - manipule o DOM com JavaScript/listaDeTarefas.js
+++ Trilha Front End/JavaScript na Web - manipule o DOM com JavaScript/listaDeTarefas.js
@@ -1,17 +1,21 @@
 const criarTarefa = (evento) => {
+
     evento.preventDefault();
+
+    const lista = document.querySelector('[data-list]');
     const input = document.querySelector('[data-form-input]'); //acessando o input
     const valor = input.value; //conteúdo do input
 
-    const tarefa = document.querySelector('[data-task]'); //acessando o parágrafo do <li>
+    const tarefa = document.createElement('li'); //criando a tag <li>
+    tarefa.classList.add('task'); //adicionando uma classe na tag <li>
+    const conteudo = `<p class="content">${valor}</p>`; //criando um novo parágrafo com conteúdo para adicionar na <li>
 
-    const conteudo = `<p class="content">${valor}</p>`; //criando um novo parágrafo com conteúdo
+    tarefa.innerHTML = conteudo; //adicionando o parágrafo criado na tag <li> criada
 
-    tarefa.innerHTML = conteudo; //adicionando o novo parágrafo criado no <li>
-
+    lista.appendChild(tarefa); //adicionando o elemento filho (tag <li>) com o parágrafo dentro do elemento pai (tag <ul>) 
     input.value = " "; //limpa o input
 }
 
-const novaTarefa = document.querySelector('[data-form-button]');
+const novaTarefa = document.querySelector('[data-form-button]'); //acessando o botão do input
 
-novaTarefa.addEventListener('click', criarTarefa);
+novaTarefa.addEventListener('click', criarTarefa); //chama o evento criar tarefa ao clicar no botão do input
