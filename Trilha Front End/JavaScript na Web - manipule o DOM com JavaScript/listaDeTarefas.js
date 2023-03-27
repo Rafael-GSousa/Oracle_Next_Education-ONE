eb46b934c074ca3c77a5564b830df0afeab38895
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-27 18:20:49 -0300

    Comportamento do formulário

--- Trilha Front End/JavaScript na Web - manipule o DOM com JavaScript/listaDeTarefas.js
+++ Trilha Front End/JavaScript na Web - manipule o DOM com JavaScript/listaDeTarefas.js
@@ -1,9 +1,11 @@
-const novaTarefa = document.querySelector('[data-form-button]');
-
-
-
-novaTarefa.addEventListener('click', () => {
+const criarTarefa = (evento) => {
+    evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
     const valor = input.value;
     console.log(valor);
-});
+    input.value = " ";
+}
+
+const novaTarefa = document.querySelector('[data-form-button]');
+
+novaTarefa.addEventListener('click', criarTarefa);
