0feeaa0ebe812d609457f4a04140c524abc29e36
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-27 19:55:56 -0300

    JavaScript IIFE

--- Trilha Front End/JavaScript na Web - manipule o DOM com JavaScript/listaDeTarefas.js
+++ Trilha Front End/JavaScript na Web - manipule o DOM com JavaScript/listaDeTarefas.js
@@ -1,46 +1,50 @@
-const criarTarefa = (evento) => { //função de adicionar um elemento na lista ao clicar no botão do input
+(() => {/*todas as funções estão dentro de uma função anônima e não mais como global, isso impede acesso a 
+    elas pelo console do browser*/
 
-    evento.preventDefault();
+    const criarTarefa = (evento) => { //função de adicionar um elemento na lista ao clicar no botão do input
 
-    const lista = document.querySelector('[data-list]');
-    const input = document.querySelector('[data-form-input]'); //acessando o input
-    const valor = input.value; //conteúdo do input
+        evento.preventDefault();
 
-    const tarefa = document.createElement('li'); //criando a tag <li>
-    tarefa.classList.add('task'); //adicionando uma classe na tag <li>
-    const conteudo = `<p class="content">${valor}</p>`; //criando um novo parágrafo com conteúdo para adicionar na <li>
+        const lista = document.querySelector('[data-list]');
+        const input = document.querySelector('[data-form-input]'); //acessando o input
+        const valor = input.value; //conteúdo do input
 
-    tarefa.innerHTML = conteudo; //adicionando o parágrafo criado na tag <li> criada
+        const tarefa = document.createElement('li'); //criando a tag <li>
+        tarefa.classList.add('task'); //adicionando uma classe na tag <li>
+        const conteudo = `<p class="content">${valor}</p>`; //criando um novo parágrafo com conteúdo para adicionar na <li>
 
-    tarefa.appendChild(botaoConclui());
-    lista.appendChild(tarefa); //adicionando o elemento filho (tag <li>) com o parágrafo dentro do elemento pai (tag <ul>) 
-    input.value = " "; //limpa o input
-}
+        tarefa.innerHTML = conteudo; //adicionando o parágrafo criado na tag <li> criada
 
-const novaTarefa = document.querySelector('[data-form-button]'); //acessando o botão do input
+        tarefa.appendChild(botaoConclui());
+        lista.appendChild(tarefa); //adicionando o elemento filho (tag <li>) com o parágrafo dentro do elemento pai (tag <ul>) 
+        input.value = " "; //limpa o input
+    }
 
-novaTarefa.addEventListener('click', criarTarefa); //chama o evento criar tarefa ao clicar no botão do input
+    const novaTarefa = document.querySelector('[data-form-button]'); //acessando o botão do input
 
-
-const botaoConclui = () => {
-    const botaoConclui = document.createElement('button'); //cria o botão
-
-    botaoConclui.classList.add('check-button');
-    botaoConclui.innerText = "concluir";
-    botaoConclui.addEventListener('click', concluirTarefa); //evento de click do botão
+    novaTarefa.addEventListener('click', criarTarefa); //chama o evento criar tarefa ao clicar no botão do input
 
 
-    return botaoConclui;
-}
+    const botaoConclui = () => {
+        const botaoConclui = document.createElement('button'); //cria o botão
+
+        botaoConclui.classList.add('check-button');
+        botaoConclui.innerText = "concluir";
+        botaoConclui.addEventListener('click', concluirTarefa); //evento de click do botão
+
+
+        return botaoConclui;
+    }
 
 
 
 
-const concluirTarefa = (evento) => {
-    const botaoConclui = evento.target;
+    const concluirTarefa = (evento) => {
+        const botaoConclui = evento.target;
 
-    const tarefaCompleta = botaoConclui.parentElement;
+        const tarefaCompleta = botaoConclui.parentElement;
 
-    tarefaCompleta.classList.toggle('done');
+        tarefaCompleta.classList.toggle('done');
 
-}
+    }
+})()
