fac66b4fca313b6750385271f2acd3a6bac0fa91
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-26 23:40:14 -0300

    Update JavaScript

---
+++ Trilha Front End/JavaScript - explorando a linguagem/8 - loops.js
@@ -0,0 +1,27 @@
+console.log('\nTrabalhando com condicionais');
+
+const listaDeDestinos = new Array(`Salvador`, `São Paulo`, `Rio de Janeiro`);
+
+
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "BUNDA";
+
+console.log('\nDestinos possíveis:');
+console.log(listaDeDestinos);
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada;
+
+let cont = 0;
+let destinoExiste = false;
+while (cont < listaDeDestinos.length) {
+  if(listaDeDestinos[cont] == destino){
+    destinoExiste = true;
+    break;
+  }
+  cont++;  
+}
+
+console.log("Destino existe: ", destinoExiste);
