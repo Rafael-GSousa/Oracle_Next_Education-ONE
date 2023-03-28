7e03626bbfbe58eca7382af173ecad7bb6cfc0b1
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-28 20:06:17 -0300

    Mostrando mensagem de erro

--- Trilha Front End/JavaScript na Web - validação de Formulários e HTML5/js/validacao.js
+++ Trilha Front End/JavaScript na Web - validação de Formulários e HTML5/js/validacao.js
@@ -1,20 +1,45 @@
 export function valida(input) {
     const tipoDeInput = input.dataset.tipo;
 
-    if(validadores[tipoDeInput]) {
+    if (validadores[tipoDeInput]) {
         validadores[tipoDeInput](input);
+    }
+
+    if (input.validity.valid) {
+        input.parentElement.classList.remove('input-container--invalido');
+    } else {
+        input.parentElement.classList.add('input-container--invalido');
     }
 }
 
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email: {
+        valueMissing: 'O campo e-mail não pode estar vazio.',
+        typeMismatch: 'O e-mail digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula e uma minúscula e não deve conter símbolos.'
+    },
+dataNascimento: {
+    valueMissing: 'O campo data de nascimento não pode estar vazio.',
+    customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+}
+
+}
+
 const validadores = {
-    dataNascimento:input => validaDataNascimento(input)
+    dataNascimento: input => validaDataNascimento(input)
 }
 
 function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value);
     let mensagem = '';
 
-    if(!maiorQue18(dataRecebida)) {
+    if (!maiorQue18(dataRecebida)) {
         mensagem = 'Você deve ser maior que 18 anos para se cadastrar.';
     }
 
