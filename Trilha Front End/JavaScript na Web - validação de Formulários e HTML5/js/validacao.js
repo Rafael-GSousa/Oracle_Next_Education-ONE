e38ce9928c59c93fbb6fdc87750a034347954bc2
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-29 19:45:46 -0300

    Calculando a soma dos dígitos

--- Trilha Front End/JavaScript na Web - validação de Formulários e HTML5/js/validacao.js
+++ Trilha Front End/JavaScript na Web - validação de Formulários e HTML5/js/validacao.js
@@ -82,7 +82,7 @@ function validaCPF(input) {
     const cpfFormatado = input.value.replace(/\D/g, '');
     let mensagem = '';
 
-    if(!checaCPFRepetido(cpfFormatado)) {
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)) {
         mensagem = 'O CPF digitado não é válido.'
     }
 
@@ -118,6 +118,27 @@ function checaEstruturaCPF(cpf) {
     const multiplicador = 10;
 
     return checaDigitoVerificador(cpf, multiplicador);
+}
+
+function checaDigitoVerificador(cpf, multiplicador) {
+    if(multiplicador >= 12) {
+        return true;
+    }
+
+    let multiplicadorInicial = multiplicador;
+    let soma = 0;
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+    const digitoVerificador = cpf.charAt(multiplicador - 1);
+    for(let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--){
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial;
+        contador++;
+    }
+
+    if (digitoVerificador == confirmaDigito(soma)) {
+        return checaDigitoVerificador(cpf, multiplicador + 1);
+    }
+
+    return false;
 }
 
 function confirmaDigito(soma) {
