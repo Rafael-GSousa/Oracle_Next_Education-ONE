05913b348fef5d4d3f64894b0a54433a17bcde6e
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-29 20:19:39 -0300

    Adicionando máscara ao CPF

---
+++ Trilha Front End/JavaScript na Web - validação de Formulários e HTML5/js/mascara.js
@@ -0,0 +1,11 @@
+const cpf = document.querySelector('[data-cpf]')
+
+cpf.addEventListener('keypress', () => {
+    let cpfTamanho = cpf.value.length
+
+    if (cpfTamanho === 3 || cpfTamanho === 7) {
+        cpf.value += '.'        
+    } else if (cpfTamanho === 11) {
+        cpf.value += '-'
+    }
+})
