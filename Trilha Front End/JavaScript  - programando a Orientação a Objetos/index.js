57460e40b0d18eb4d1e207e24cdfe8eec0b07ba3
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-31 16:55:59 -0300

    Comportamento de classes

--- Trilha Front End/JavaScript  - programando a Orientação a Objetos/index.js
+++ Trilha Front End/JavaScript  - programando a Orientação a Objetos/index.js
@@ -1,27 +1,50 @@
 // criando uma classe de molde com suas propriedades
-class Cliente{
+class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
-    rg;
+
+    // método sacar foi adicionado dentro da classe ContaCorrente
+    // o "this" define que a conta corrente a ser tratada é a que estiver em uso  no momento 
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            console.log('Você retirou R$ ' + valor);
+        }else{
+            console.log('Valor solicitado é maior que o disponível. Saque não processado!');
+        }
+    }
 }
 
 // instanciando a classe (criando objetos)
-const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 // atribuindo valores para cada atributo do objeto
+const cliente1 = new Cliente();
 cliente1.nome = 'Ricardo';
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
-cliente1.rg = 123456789;
 
+const cliente2 = new Cliente();
 cliente2.nome = 'Alice';
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(50);
+console.log(contaCorrenteRicardo.saldo);
+
+
+const contaCorrenteAlice = new ContaCorrente();
+contaCorrenteAlice.saldo = 0;
+contaCorrenteAlice.agencia = 1001;
 
 // imprimindo os objetos com seus atributos no PowerShell utilizando NodeJS
-console.log(cliente1, cliente2);
+console.log(cliente1);
+console.log(cliente2);
