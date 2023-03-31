cb34e93accbbc2351d8388905acc5c19d08eba65
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-31 15:18:39 -0300

    Entendendo o problema do cliente

---
+++ Trilha Front End/JavaScript  - programando a Orientação a Objetos/index.js
@@ -0,0 +1,11 @@
+const cliente1Nome = 'Ricardo';
+const cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = 'Alice';
+const cliente2CPF = 88822233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome,'\n'+cliente1CPF,'\n'+cliente1Agencia,'\n'+cliente1Saldo);
