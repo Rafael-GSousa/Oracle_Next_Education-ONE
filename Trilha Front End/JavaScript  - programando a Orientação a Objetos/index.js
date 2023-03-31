1e808449d1b94cddb56698472d3ff085a150dc6e
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-31 15:35:15 -0300

    Nossa primeira classe

--- Trilha Front End/JavaScript  - programando a Orientação a Objetos/index.js
+++ Trilha Front End/JavaScript  - programando a Orientação a Objetos/index.js
@@ -1,11 +1,23 @@
-const cliente1Nome = 'Ricardo';
-const cliente1CPF = 11122233309;
-const cliente1Agencia = 1001;
-const cliente1Saldo = 0;
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+    rg;
+}
 
-const cliente2Nome = 'Alice';
-const cliente2CPF = 88822233309;
-const cliente2Agencia = 1001;
-const cliente2Saldo = 0;
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
 
-console.log(cliente1Nome,'\n'+cliente1CPF,'\n'+cliente1Agencia,'\n'+cliente1Saldo);
+cliente1.nome = 'Ricardo';
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+cliente1.rg = 123456789;
+
+cliente2.nome = 'Alice';
+cliente2.cpf = 88822233309;
+cliente2.agencia = 1001;
+cliente2.saldo = 0;
+
+console.log(cliente1, cliente2);
