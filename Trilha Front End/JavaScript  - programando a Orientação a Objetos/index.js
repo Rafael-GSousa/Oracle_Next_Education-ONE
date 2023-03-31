34371fe2a4247301881f93e391adf780a6071f4e
Rafael-GSousa <rafael.gsousa@outlook.com>
2023-03-31 15:39:58 -0300

    Nossa primeira classe

--- Trilha Front End/JavaScript  - programando a Orientação a Objetos/index.js
+++ Trilha Front End/JavaScript  - programando a Orientação a Objetos/index.js
@@ -1,3 +1,4 @@
+// criando uma classe de molde com suas propriedades
 class Cliente{
     nome;
     cpf;
@@ -6,9 +7,11 @@ class Cliente{
     rg;
 }
 
+// instanciando a classe (criando objetos)
 const cliente1 = new Cliente();
 const cliente2 = new Cliente();
 
+// atribuindo valores para cada atributo do objeto
 cliente1.nome = 'Ricardo';
 cliente1.cpf = 11122233309;
 cliente1.agencia = 1001;
@@ -20,4 +23,5 @@ cliente2.cpf = 88822233309;
 cliente2.agencia = 1001;
 cliente2.saldo = 0;
 
+// imprimindo os objetos com seus atributos no PowerShell utilizando NodeJS
 console.log(cliente1, cliente2);
